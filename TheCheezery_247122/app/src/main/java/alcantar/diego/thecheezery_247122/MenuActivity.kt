49ad8196d3a4c672c4c3e3856b708ea0606f3aa4
Diego <haloalcantar@gmail.com>
2025-02-11 21:44:28 -0700

    Practica 5 - The Cheezery has products
    Segundo Avance de The Cheezery

--- TheCheezery_247122/app/src/main/java/alcantar/diego/thecheezery_247122/MenuActivity.kt
+++ TheCheezery_247122/app/src/main/java/alcantar/diego/thecheezery_247122/MenuActivity.kt
@@ -1,6 +1,8 @@
 package alcantar.diego.thecheezery_247122
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,35 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweet: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+
+        btnCold.setOnClickListener(){
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "ColdDrinks") // Cambia según la categoría deseada
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener(){
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "HotDrinks") // Cambia según la categoría deseada
+            startActivity(intent)
+        }
+
+        btnSweet.setOnClickListener(){
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "Sweets") // Cambia según la categoría deseada
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener(){
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("categoria", "Salties") // Cambia según la categoría deseada
+            startActivity(intent)
         }
     }
 }
---
+++ TheCheezery_247122/app/src/main/java/alcantar/diego/thecheezery_247122/Product.kt
@@ -0,0 +1,8 @@
+package alcantar.diego.thecheezery_247122
+
+data class Product(
+    var name: String,
+    var image: Int,
+    var description: String,
+    var price: Double
+)
